5d5cb26c74ee98036a439d0efdd2743db56dd1f5
MarkTBSS <marktbss@gmail.com>
2023-07-13 14:45:22 +0700

    add 01_Solve_Me_First.js

---
+++ 01_Solve_Me_First.js
@@ -0,0 +1,12 @@
+function solveMeFirst(a, b) {
+    var result = a + b;
+    return result;
+}
+
+console.log(solveMeFirst(3, 7))
+
+// shorter version
+/* function solveMeFirst(a, b) {
+    // Hint: Type return a+b below
+    return a + b
+} */
